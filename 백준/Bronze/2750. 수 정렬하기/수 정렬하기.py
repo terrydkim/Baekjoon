3f7e52e840fc250707ff2c5dab66601a823f7b88
Terry <112925579+terrydkim@users.noreply.github.com>
2023-03-04 20:41:22 +0900

    [Bronze II] Title: 수 정렬하기, Time: 192 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -5,10 +5,10 @@ a = int(input())
 b = [int(sys.stdin.readline().strip()) for i in range(a)]
 
 for i in range(len(b)-1):
-    for j in range(len(b)-1):
-        if b[j] > b[j+1]:
-            b[j],b[j+1] = b[j+1],b[j]
+    for j in range(len(b)-1,i,-1):
+        if b[j-1] > b[j]:
+            b[j-1],b[j] = b[j],b[j-1]
 
 
-for i in range(len(b)):
-    print(b[i])
+for i in b:
+    print(i)
